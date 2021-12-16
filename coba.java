e5d98338e90d640d84a1c680d75bd81e019dc946
robyazharii <74696501+robyazharii@users.noreply.github.com>
2021-12-16 10:59:33 +0700

    Tugas  5
    Roby Azhari
    Muhammad Wildan
    Hanif Zakiy

---
+++ coba.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class coba {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        boolean lanjutkanInput = true;
+
+        do{
+            try{
+
+                System.out.print("Masukkan nilai integer pertama nya pak: ");
+                int angkaPertama = input.nextInt();
+                System.out.println("Nilai integer pertama adalah: " + angkaPertama);
+                System.out.print("Masukkan nilai integer kedua nya pak: ");
+                int angkaKedua = input.nextInt();
+                lanjutkanInput = false;
+
+                System.out.println("Nilai integer kedua adalah: " + angkaKedua);
+                System.out.println("Jumlah nilai integer adalah: " + (angkaPertama + angkaKedua));
+
+            }
+
+            catch (Exception ex){
+
+                System.out.println("Silakan coba lagi. (" + "Input yang dimasukkan tidak sesuai: "
+                        + "nilai harus berupa integer!)");
+                input.nextLine(); //Akan membuang input saat ini sehingga user dapat memasukkan input yang baru
+            }
+
+        }while(lanjutkanInput);
+
+    }
+}
